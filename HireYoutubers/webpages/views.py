62d83247bd9410b7b72b7fab1f830a557faf23a0
kisho1131 <sumitkishore0131@gmail.com>
2021-07-22 22:01:11 +0530

    Updating Home Page

--- HireYoutubers/webpages/views.py
+++ HireYoutubers/webpages/views.py
@@ -7,10 +7,12 @@ def home(request):
     sliders = Slider.objects.all() 
     teams = Team.objects.all()
     featured_youtubers = Youtuber.objects.order_by('-created_date').filter(is_featured = True)
+    all_youtubers = Youtuber.objects.order_by('-created_date').filter(is_featured = False)
     data = {
         'sliders':sliders,
         'teams' : teams,
         'featured_youtubers' : featured_youtubers,
+        'all_tubers' : all_youtubers,
     }
     return render(request, 'webpages/home.html', data)
 
